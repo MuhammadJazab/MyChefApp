bd8f8d1ff47dcb85cce9f3ddba99f67789ae9dde
Muhammad Jazab Najeeb <jazab.najeb@datamagnetics.com>
2020-10-14 19:04:26 +0500

    add splash screen for android

---
+++ MyChefApp/MyChefApp.Android/SplashScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Android;
+using Android.App;
+using Android.OS;
+
+namespace MyChefApp.Droid
+{
+    [Activity(MainLauncher = true, NoHistory = true, ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
+    public class SplashScreen : Activity
+    {
+        protected async override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.SplashScreen);
+
+            await Task.Delay(500);
+
+            StartActivity(typeof(MainActivity));
+            Finish();
+        }
+    }
+}
